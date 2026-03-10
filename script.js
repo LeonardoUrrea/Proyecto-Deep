7b99f9dae9c6211a94f7bda3fd34e433fb873746
LeonardoUrrea <leonardo.urrea.m@gmail.com>
2026-03-09 23:54:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Proyecto Deep cargado correctamente en GitHub Pages.");
 
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(link => {
+    link.addEventListener("click", event => {
+      const targetId = link.getAttribute("href");
+      const target = document.querySelector(targetId);
+
+      if (target) {
+        event.preventDefault();
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+    });
+  });
+});
